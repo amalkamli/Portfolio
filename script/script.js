8704383370fbd30243d67b0f30fe4aff04639b33
core.pager=cat <amal.kemly@gmail.com>
2025-03-16 00:08:23 +0100

    Re-deploy GitHub Pages

--- script/script.js
+++ script/script.js
@@ -22,44 +22,6 @@ function handleNavbarCollapse() {
     });
   });
 }
-
-// Function to dynamically create HTML elements from the JSON file
-function createSkillsFromJSON() {
-  const container = document.querySelector("#skills .container");
-  let row = document.createElement("div");
-  row.classList.add("row");
-
-  // Load the JSON file
-  fetch("data/skills.json")
-    .then((response) => response.json())
-    .then((data) => {
-      // Iterate through the JSON data and create HTML elements
-      data.forEach((item, index) => {
-        const card = document.createElement("div");
-        card.classList.add("col-lg-4", "mt-4");
-        card.innerHTML = `
-                    <div class="card skillsText">
-                        <div class="card-body">
-                            <img src="./images/${item.image}" alt="${item.description}" />
-                            
-                            <h4 class="card-title mt-3">${item.title}</h4>
-                            <p class="card-text mt-3">${item.text}</p>
-                        </div>
-                    </div>
-                `;
-
-        // Append the card to the current row
-        row.appendChild(card);
-
-        // If the index is a multiple of 3 or it's the last element, create a new row
-        if ((index + 1) % 3 === 0 || index === data.length - 1) {
-          container.appendChild(row);
-          row = document.createElement("div");
-          row.classList.add("row");
-        }
-      });
-    });
-}
 // Function to dynamically create HTML elements from the JSON file
 function createPortfolioFromJSON() {
   const container = document.querySelector("#portfolio .container");
@@ -99,6 +61,44 @@ function createPortfolioFromJSON() {
       });
     });
 }
+// Function to dynamically create HTML elements from the JSON file
+function createSkillsFromJSON() {
+  const container = document.querySelector("#skills .container");
+  let row = document.createElement("div");
+  row.classList.add("row");
+
+  // Load the JSON file
+  fetch("data/skills.json")
+    .then((response) => response.json())
+    .then((data) => {
+      // Iterate through the JSON data and create HTML elements
+      data.forEach((item, index) => {
+        const card = document.createElement("div");
+        card.classList.add("col-lg-4", "mt-4");
+        card.innerHTML = `
+                    <div class="card skillsText">
+                        <div class="card-body">
+                            <img src="./images/${item.image}" alt="${item.description}" />
+                            
+                            <h4 class="card-title mt-3">${item.title}</h4>
+                            <p class="card-text mt-3">${item.text}</p>
+                        </div>
+                    </div>
+                `;
+
+        // Append the card to the current row
+        row.appendChild(card);
+
+        // If the index is a multiple of 3 or it's the last element, create a new row
+        if ((index + 1) % 3 === 0 || index === data.length - 1) {
+          container.appendChild(row);
+          row = document.createElement("div");
+          row.classList.add("row");
+        }
+      });
+    });
+}
+
 
 // Appelez la fonction pour exécuter le code
 createPortfolioFromJSON();
